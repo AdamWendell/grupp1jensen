4f5285c731217cf833dfca7e006c05ab21e7b547
AdamWendell <Adam@websmith.se>
2016-03-14 09:31:37 +0100

    Comments in bokningar.js

--- routes/admin/bokningar.js
+++ routes/admin/bokningar.js
@@ -4,14 +4,19 @@ var BokningModel = require('../../model/BokningModel');
 var BilarModel = require('../../model/BilarModel');
 /* GET home page. */
 router.get('/bokningar', function(req, res, next) {
+  // Hämta alla bokningar
   BokningModel.find({}).then((bokningar) => {
     // console.log(bokningar);
+    // Hämta endast de bilar som är bokade
     BilarModel.find({bil: {$in: bokningar.map( (bokning) => bokning.bil)}}).then((bilar) => {
       console.log(bokningar);
+      // Gör arrays där bokningar paras med den bokade bilen.
       var bokBilarr = bokningar.map((bokning, i) => [bokning, bilar.filter( (bil) => bil.bil == bokning.bil)[0]]);
-
+      // Tidigare bokningar är bilar som tom datum har vart eller att bilen är återlämnad
       var tidBok = bokBilarr.filter((bokning) => Date.parse(bokning[0].tom) < Date.now()  || bokning[0].aterlamnad)
+      // Kommande bokningar
       var komBok = bokBilarr.filter((bokning) => Date.parse(bokning[0].from) > Date.now() )
+      // Aktiva boknningar
       var aktBok = bokBilarr.filter((bokning) => Date.parse(bokning[0].tom) > Date.now() && Date.parse(bokning[0].from) < Date.now() && !bokning[0].aterlamnad )
       console.log('tidBok');
       console.log(tidBok[0][0]);
